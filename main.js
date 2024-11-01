a40533583a23ef8958bc5952810d06083bfd1f82
DestinyFrog <107517847+DestinyFrog@users.noreply.github.com>
2024-11-01 19:27:56 -0300

    fix main

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 import './style.css'
 import icons from './features/icons.js'
 import Atom from './models/atom.js'
-import WinPeriodicTable from './windows/winPeriodicTable.js'
 
 ( async () => {
 	await Atom.loadAll()
 
 	const grid_app = document.createElement('footer')
 
-	icons.forEach(({action, title, icon, color}) => {
+	icons.forEach(({action, icon}) => {
 		const but = document.createElement('button')
 		but.className = 'app-icon'
 		but.addEventListener('click', action)
@@ -17,13 +16,8 @@ import WinPeriodicTable from './windows/winPeriodicTable.js'
 		const img = document.createElement('img')
 		img.src = icon
 		but.appendChild(img)
-
-		const t = document.createElement('p')
-		t.textContent = title
-		// but.appendChild(t)
 	})
 
 	document.getElementById('app').appendChild(grid_app)
-
 } ).call(this)
 
